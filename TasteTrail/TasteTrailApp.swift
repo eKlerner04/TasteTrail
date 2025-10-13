854cc8a310fea120159d58b7df64c70f376cce16
eKlerner04 <emilklerner@icloud.com>
2025-10-13 12:33:21 +0300

    Initial Commit

---
+++ TasteTrail/TasteTrailApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasteTrailApp.swift
+//  TasteTrail
+//
+//  Created by Emil Klerner on 13.10.25.
+//
+
+import SwiftUI
+
+@main
+struct TasteTrailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
